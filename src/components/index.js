1a127beac760d189e28f46850ae0f07682c38f96
aryansenpaii <aryansenpai256@gmail.com>
2023-06-22 21:49:13 +0530

    imported button component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Details from "./Details";
 import Login from "./Login";
 import Navbar from './Navbar';
+import ButtonLogin from "./ButtonLogin";
 
 export{
     Details,
     Login,
-    Navbar
+    Navbar,
+    ButtonLogin
 };
