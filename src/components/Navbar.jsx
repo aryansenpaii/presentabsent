013be221d1b2e93da7b33a715f32e1f764e320d4
Bharattaneja01 <bharattaneja73@gmail.com>
2023-06-26 23:25:18 +0530

    Created navbar and added postcss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import {navLinks} from '../constants';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-[#f4eeff]`}>
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+
+        ))}
+      </ul>
+    </nav>
   )
 }
 
