7f64c8ea59246d81da3b1ef40a8363ae4bd8c7ff
Bharattaneja01 <bharattaneja73@gmail.com>
2023-06-29 23:41:13 +0530

    created dashboard file

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import LoginCarousel from "./LoginCarousel";
 import Login from "./Login";
 import Navbar from './Navbar';
 import ButtonLogin from "./ButtonLogin";
+import Dashboard from "./Dashboard";
 
 export{
     LoginCarousel,
     Login,
     Navbar,
-    ButtonLogin
+    ButtonLogin,
+    Dashboard
 };
